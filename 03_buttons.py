0441d43ab793b68684343c3fea2e04474cfeca0f
Colin White <cwhite@torchtechnologies.com>
2020-04-06 11:29:42 -0500

    deletes dead code and tries to conform to PEP8

--- 03_buttons.py
+++ 03_buttons.py
@@ -18,15 +18,4 @@ label.pack()
 button = tk.Button(root, text='Tkinter Button', command=callback)
 button.pack()
 
-'''
-label['text'] = 'An updated label'
-label.config(text='Another way to update a label', fg='red')
-label.config(bg='yellow')
-label.config(font='Times 15')
-label.config(text='Final update to label')
-label.config(wraplength='100')
-label.config(justify=tk.LEFT) # Default is center, it seems
-label.pack()
-'''
-
 root.mainloop()
--- 04_entry_widgets.py
+++ 04_entry_widgets.py
@@ -37,18 +37,7 @@ def callback():
 label = tk.Label(root, text='Button Testing')
 label.pack()
 
-button = tk.Button(root, text='Tkinter Button', command=callback)
+button = tk.Button(root, text='Activation Button', command=callback)
 button.pack()
-
-'''
-label['text'] = 'An updated label'
-label.config(text='Another way to update a label', fg='red')
-label.config(bg='yellow')
-label.config(font='Times 15')
-label.config(text='Final update to label')
-label.config(wraplength='100')
-label.config(justify=tk.LEFT) # Default is center, it seems
-label.pack()
-'''
 
 root.mainloop()
